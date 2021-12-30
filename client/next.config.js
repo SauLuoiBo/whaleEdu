2a73a96dd9b7889e7b8e282911c5ca628bad7884
SauLuoi <dai.dotien@gmail.com>
2021-12-30 19:44:11 +0700

    h

--- client/next.config.js
+++ client/next.config.js
@@ -1,29 +1,15 @@
-const routes = require("./configs/router");
-
-module.exports = {
-  // reactStrictMode: true,
-};
-
 const withTM = require("next-transpile-modules")([
   "@mui/material",
   "@mui/system",
   "@mui/icons-material", // If @mui/icons-material is being used
 ]);
 
-// module.exports = withTM({
-//   webpack: (config) => {
-//     config.resolve.alias = {
-//       ...config.resolve.alias,
-//       "@mui/styled-engine": "@mui/styled-engine-sc",
-//     };
-//     return config;
-//   },
-// });
-
-module.exports = {
+module.exports = withTM({
   webpack: (config) => {
-    config.resolve.alias["@material-ui/styled-engine"] =
-      "@material-ui/styled-engine-sc";
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@mui/styled-engine": "@mui/styled-engine-sc",
+    };
     return config;
   },
-};
+});
